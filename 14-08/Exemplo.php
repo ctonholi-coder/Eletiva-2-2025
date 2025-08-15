200cf5a44e724098d64c95d17580e09a544682f5
camilla-coder <ctonholi@gmail.com>
2025-08-14 21:00:47 -0300

    Início php

---
+++ 14-08/Exemplo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- responsável pela responsividade -->
+    <title>Primeiro Exemplo</title>
+</head>
+<body>
+    <?php 
+        $dia = date("d"); 
+    ?>
+    <h1>Hoje é dia <?= $dia ?> de Agosto de 2025</h1>
+    
+
+</body>
+</html>
