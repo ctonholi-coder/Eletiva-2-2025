230d06b4cb361422867aca72ec162ba526a359d5
camilla-coder <ctonholi@gmail.com>
2025-08-21 20:09:51 -0300

    Exemplo alterado

--- 21-08/exemplo1.php
+++ 21-08/exemplo1.php
@@ -9,7 +9,7 @@
 <body> 
 <div class="container">
 <h1>Exemplo - Soma de Valores</h1>
-<form method="post" action="resposta_exemplo1.php">
+<form method="post">
 <div class="mb-3">
               <label for="valor1" class="form-label">Informe o primeiro valor</label>
               <input type="number" id="valor1" name="valor1" class="form-control" required="">
@@ -19,6 +19,20 @@
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $valor1 = $_POST["valor1"];
+      $valor2 = $_POST["valor2"];
+      $soma = $valor1 + $valor2;
+      echo "<p>Soma: $soma</p>";
+//- subtração  * multiplicação **elevado / divisao //divisão % resto da divisão</p>";
+
+      
+  }
+
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
