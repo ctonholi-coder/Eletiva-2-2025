bdc1e2ca89d7943befbe92863076f81c635a11ec
camilla-coder <ctonholi@gmail.com>
2025-08-28 20:40:42 -0300

    aula28

---
+++ 28-08/estrutura5.php
@@ -0,0 +1,10 @@
+    <?php
+        include("cabecalho.php");
+
+        $i = 1;
+        do {
+            echo "O numero é: $i <br>";
+            $i++;
+        } while ($i <= 5);
+        
+        include("rodape.php");
