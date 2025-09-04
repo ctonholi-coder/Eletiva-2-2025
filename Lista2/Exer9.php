9490d3ba061ff483cf18157027b79934c804db44
camilla-coder <ctonholi@gmail.com>
2025-09-04 19:39:24 -0300

    Exer5e9

---
+++ Lista2/Exer9.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 9 - Lista 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 9 - Lista 2</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="numero1" class="form-label">Informe um numero</label>
+              <input type="text" id="numero1" name="numero1" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $numero1 = $_POST["numero1"];
+            $fatorial = $numero1;
+            for ($i = $numero1-1; $i>1; $i--) {
+                $fatorial = $numero1;
+                $fatorial = $fatorial * $i;
+                }
+                echo "O fatorial de $numero1 é $fatorial";
+        }
+        ?>  
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
